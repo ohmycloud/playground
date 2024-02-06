23754d205dc015f3e86a40ea9cbddef996973532
ohmycloud <ohmycloudy@gmail.com>
2024-02-06 23:53:38 +0800

    Implement `Iterator` for `Fibonacci`.

---
+++ fibonacci.rs
@@ -0,0 +1,25 @@
+struct Fibonacci {
+    curr: u32,
+    next: u32,
+}
+
+impl Iterator for Fibonacci {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = self.curr;
+        self.curr = self.next;
+        self.next = current + self.next;
+        Some(current)
+    }
+}
+
+fn fibonacci() -> Fibonacci {
+    Fibonacci { curr: 0, next: 1}
+}
+
+fn main() {
+    for i in fibonacci().skip(4).take(4) {
+        println!("{}", i);
+    }
+}
