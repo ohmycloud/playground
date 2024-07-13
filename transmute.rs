6deb9ba158da520ab96c3b4295256f6539f8c922
ohmycloud <ohmycloudy@gmail.com>
2024-07-13 17:16:03 +0800

    env var

---
+++ transmute.rs
@@ -0,0 +1,11 @@
+fn main() {
+    use std::mem::transmute;
+
+    struct User {
+        name: String,
+        number: u32,
+    }
+
+    let some_i32s = [1,2,3,4,5,6,7,8];
+    let user = unsafe { transmute::<[i32; 8], User>(some_i32s )};
+}
